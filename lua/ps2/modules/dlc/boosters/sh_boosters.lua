e4ce3cd618ea9c0dbdba84eb44e078bcc3e68ab0
Valentin Funk <funk.valentin@gmail.com>
2015-07-25 16:55:22 +0200

    FIx speed booster errorign

--- lua/ps2/modules/dlc/boosters/sh_boosters.lua
+++ lua/ps2/modules/dlc/boosters/sh_boosters.lua
@@ -1,11 +1,13 @@
 Pointshop2.BoosterTypes = {}
+Pointshop2.BoosterAlt = {}
 
 function Pointshop2.AddBoosterType( tbl )
 	Pointshop2.BoosterTypes[tbl.Name] = tbl
+	Pointshop2.BoosterAlt[tbl.AltName] = tbl
 end
 
 function Pointshop2.GetBoosterByName( name )
-	return Pointshop2.BoosterTypes[name]
+	return Pointshop2.BoosterTypes[name] or Pointshop2.BoosterAlt[name]
 end
 
 function Pointshop2.BoostDrainWhenAlive( item, ply )
