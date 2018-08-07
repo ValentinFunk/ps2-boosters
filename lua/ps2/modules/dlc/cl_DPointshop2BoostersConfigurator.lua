0df70c40a96c330fc9c9e1f40a43d3c04f7c5232
Valentin Funk <funk.valentin@gmail.com>
2018-08-07 08:48:25 +0200

    Update cl_DPointshop2BoostersConfigurator.lua

--- lua/ps2/modules/dlc/cl_DPointshop2BoostersConfigurator.lua
+++ lua/ps2/modules/dlc/cl_DPointshop2BoostersConfigurator.lua
@@ -2,7 +2,7 @@ local PANEL = {}
 
 function PANEL:Init( )
 	self:SetSkin( Pointshop2.Config.DermaSkin )
-	self:SetTitle( "Pointshop2 Gambling Settings" )
+	self:SetTitle( "Pointshop2 Boosters Settings" )
 	self:SetSize( 300, 600 )
 	
 	self:AutoAddSettingsTable( { BoosterSettings = Pointshop2.GetModule( "Pointshop 2 DLC" ).Settings.Shared.BoosterSettings }, self )
